475226203e278215b0234950fd7a5e887a8b3a25
Remy Dahlke <rdahlke@amazon.com>
2025-04-29 21:31:41 -0400

    s

--- scripts/provision_nixos_ec2.sh
+++ scripts/provision_nixos_ec2.sh
@@ -1,16 +1,25 @@
-aws ec2 import-key-pair \
-  --key-name "id_rsa" \
-  --public-key-material fileb://~/.ssh/id_rsa.pub
+#!/bin/bash
+set -e
 
+aws --version
 
-aws ec2 describe-images \
-    --owners 427812963091 \
-    --region us-east-1 \
-    --filter \
-        'Name=name,Values=nixos/*' \
-        'Name=architecture,Values=x86_64' \
-    --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId'
-    --output text
+if [ -z "$(aws ec2 describe-key-pairs --filters "Name=key-name,Values=id_rsa" --output text)" ]; then
+    aws ec2 import-key-pair \
+        --key-name "id_rsa" \
+        --public-key-material fileb://~/.ssh/id_rsa.pub
+fi
+
+# https://nixos.github.io/amis/
+NIXOS_IMAGE_ID=$( \
+    aws ec2 describe-images \
+        --owners 427812963091 \
+        --region us-east-1 \
+        --filter \
+            'Name=name,Values=nixos/*' \
+            'Name=architecture,Values=x86_64' \
+        --query 'reverse(sort_by(Images, &CreationDate))[0].ImageId'
+        --output text \
+)
 
 
 aws ec2 create-security-group \
@@ -27,7 +36,7 @@ aws ec2 run-instances \
     --image-id "ami-0ccb2c640cdb2ee19" \
     --instance-type "m7a.2xlarge" \
     --key-name "id_rsa" \
-    --block-device-mappings '{"DeviceName":"/dev/xvda","Ebs":{"Encrypted":false,"DeleteOnTermination":true,"Iops":3000,"SnapshotId":"snap-00b099bde6384c6fb","VolumeSize":512,"VolumeType":"gp3","Throughput":125}}' \
+    --block-device-mappings '{"DeviceName":"/dev/xvda","Ebs":{"Encrypted":false,"DeleteOnTermination":true,"Iops":3000,"VolumeSize":512,"VolumeType":"gp3","Throughput":125}}' \
     --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-preview-1"]}' \
     --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"nixos-srv-2"}]}' \
     --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
