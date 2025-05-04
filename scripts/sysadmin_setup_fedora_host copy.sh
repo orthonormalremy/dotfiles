1325a16fb592bbf1a1cc23a6dd01d3673f3c40c0
Remy Dahlke <rdahlke@amazon.com>
2025-05-04 17:16:12 -0400

    s

--- scripts/sysadmin_setup_fedora_host copy.sh
+++ scripts/sysadmin_setup_fedora_host copy.sh
@@ -5,4 +5,5 @@ useradd -m rdahlke
 passwd -d rdahlke # no password
 echo "rdahlke ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/rdahlke && chmod 440 /etc/sudoers.d/rdahlke
 
+dnf upgrade -y --refresh
 dnf install -y git
