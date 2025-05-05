803de6bf491a09f607947693c799c9878986a3aa
Remy Dahlke <rdahlke@amazon.com>
2025-05-05 10:04:32 -0400

    s

--- scripts/sysadmin_setup_new_fedora_host.sh
+++ scripts/sysadmin_setup_new_fedora_host.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-useradd -m rdahlke
-passwd -d rdahlke # no password
-echo "rdahlke ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/rdahlke && chmod 440 /etc/sudoers.d/rdahlke
+id rdahlke &>/dev/null || useradd -m rdahlke
+passwd -d rdahlke
+echo "rdahlke ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/rdahlke && chmod 440 /etc/sudoers.d/rdahlke
 
 dnf upgrade -y --refresh
 dnf group install -y development-tools
