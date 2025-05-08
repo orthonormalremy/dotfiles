70e2c10d5c6ed7cf0fdc93bfd8be150d4b6aba3d
rdahlke <rdahlke@amazon.com>
2025-05-08 15:43:06 -0400

    s

--- scripts/sysadmin_setup_new_fedora_host.sh
+++ scripts/sysadmin_setup_new_fedora_host.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+# install system level packages
+dnf upgrade -y --refresh
+dnf group install -y development-tools || dnf group install -y "Development Tools"
+dnf install -y openssl-devel shadow-utils
+
+# init rdahlke user
 id rdahlke &>/dev/null || useradd -m rdahlke
 passwd -d rdahlke
 echo "rdahlke ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/rdahlke && chmod 440 /etc/sudoers.d/rdahlke
-
-dnf upgrade -y --refresh
-dnf group install -y development-tools
-dnf install -y openssl-devel
