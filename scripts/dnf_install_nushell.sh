8926931278e471d0c0a253428839dc62f12fcf60
rdahlke <rdahlke@amazon.com>
2025-05-09 16:21:16 -0400

    s

---
+++ scripts/dnf_install_nushell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "[gemfury-nushell]
+name=Gemfury Nushell Repo
+baseurl=https://yum.fury.io/nushell/
+enabled=1
+gpgcheck=0
+gpgkey=https://yum.fury.io/nushell/gpg.key" | sudo tee /etc/yum.repos.d/fury-nushell.repo
+sudo dnf install -y nushell
