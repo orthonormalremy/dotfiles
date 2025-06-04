5bc13b2888f39fce9c087b80d20a98b65dc97def
rdahlke <rdahlke@amazon.com>
2025-06-04 14:24:00 -0400

    s

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -22,7 +22,12 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+            ./home.nix
+            {
+                home.file.".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink "/home/cloudshell-user/dotfiles/.config/home-manager";
+            }
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
