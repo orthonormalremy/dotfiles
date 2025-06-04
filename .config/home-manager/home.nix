fd42b70900ca305ea183c891a8ef606d02a2e26e
rdahlke <rdahlke@amazon.com>
2025-06-04 14:32:04 -0400

    s

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -39,6 +39,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    # Create symlink to allow `home-manager switch` to work from default location
+    ".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/home-manager";
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
