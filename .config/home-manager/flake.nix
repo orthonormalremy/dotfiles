3b8e9b438c17177403f4fb38b9cc3f4f3c3faef6
rdahlke <rdahlke@amazon.com>
2025-06-05 02:09:21 -0400

    s

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -12,7 +12,7 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
+      system = builtins.currentSystem;
       pkgs = nixpkgs.legacyPackages.${system};
       username = builtins.getEnv "USER";
     in
