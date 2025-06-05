edb52998bb348e26eec85c44b9dd46b1e0c39093
rdahlke <rdahlke@amazon.com>
2025-06-05 01:59:12 -0400

    s

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -14,8 +14,6 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      
-      # builtins.getEnv requires --impure flag
       username = builtins.getEnv "USER";
     in
     {
