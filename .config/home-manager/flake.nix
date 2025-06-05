36f8e51c6cd51a061ed8616c6db18de14a1386de
rdahlke <rdahlke@amazon.com>
2025-06-04 20:48:45 -0400

    s

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -1,19 +1,21 @@
 {
-    description = "Home Manager configuration";
-
-    inputs = {
-        # Specify the source of Home Manager and Nixpkgs.
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { nixpkgs, home-manager, ... }: {
-        homeConfigurations.default = home-manager.lib.homeManagerConfiguration {
-            pkgs = nixpkgs.legacyPackages.${builtins.currentSystem};
-            modules = [ ./home.nix ];
-        };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = builtins.currentSystem;
+      pkgs = nixpkgs.legacyPackages.${system};
+      username = builtins.getEnv "USER";
+    in {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
     };
 }
