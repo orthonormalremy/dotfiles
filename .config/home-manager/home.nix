4bd848758e370c6fbdf9c4a85b312240f9900230
rdahlke <rdahlke@amazon.com>
2025-06-12 18:37:13 -0400

    s

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -6,16 +6,9 @@
   home.username = builtins.getEnv "USER";
   home.homeDirectory = builtins.getEnv "HOME";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  # `home.stateVersion` handled in ./home.init.nix
 
- # Allow unfree packages
+  # allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
